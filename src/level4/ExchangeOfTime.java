5a725a0494f3a1f3f12d1c7f3b383d2f546488c2
2564800726 <2564800726@qq.com>
2018-10-28 20:33:02 +0800

    时间戳的转换

--- src/level4/ExchangeOfTime.java
+++ src/level4/ExchangeOfTime.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ExchangeOfTime {
     private long timestamp;
     private String time;
+    // 定义一个数组，存储从年初到某月份有多少天
     private int[] dayToMonth = {
             0,
             31,
@@ -20,17 +21,30 @@ public class ExchangeOfTime {
             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
             31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31,
     };
+    public ExchangeOfTime(String time, long timestamp) {
+        this.time = time;
+        this.timestamp = timestamp;
+    }
 
-    public void startExchange() {
-        long timestamp = 0;
-        String time = null;
-        String currentType = null;
-        String aimType = null;
-        String command = null;
+    ExchangeOfTime() {
+    }
+
+    void startExchange() {
+        long timestamp;
+        String time;
+        String currentType;
+        String aimType;
+        String command;
         Scanner scan = new Scanner(System.in);
-        System.out.println("输入格式(1,2018-10-26 21:02:32,ZH,ZH 或者 2,12345678901,ZH,ZH)\nZH,ZH，前者表示当前地方时间，后者表示希望转换成的地方的时间\nZH：北京时间\nU.S：英国时间\nU.S.A：美国时间\n退出(exit):");
+        System.out.println("输入格式(1,2018-10-26 21:02:32,ZH,ZH 或者 2,12345678901,ZH,ZH)\n" +
+                "ZH,ZH，前者表示当前地方时间，后者表示希望转换成的地方的时间\n" +
+                "ZH：北京时间\n" +
+                "U.S：英国时间\n" +
+                "U.S.A：美国时间\n" +
+                "退出(exit):");
         while (true) {
             command = scan.nextLine();
+            // 将输入的命令中的字符串参数截取出来
             if (command.equals("exit")) {
                 return;
             } else if ("1".equals(command.split(",")[0])) {
@@ -48,13 +62,117 @@ public class ExchangeOfTime {
             }
         }
     }
-    public ExchangeOfTime(String time, long timestamp) {
-        this.time = time;
+
+    // 把时间戳转化为标准时间格式
+    private void timestampToTime(long timestamp, String currentType, String aimType) {
         this.timestamp = timestamp;
+        long days = this.timestamp / (24 * 60 * 60);  // 时间戳中包含的整天
+        int leftHour = (int) this.timestamp % (24 * 60 * 60) / (60 * 60);  // 时间戳中包含的整时
+        int leftMinute = (int) this.timestamp % (24 * 60 * 60) % (60 * 60) / 60;  // 时间戳中包含的整分
+        int year = 1970;
+        int month = 0;
+        int second = (int) this.timestamp % (24 * 60 * 60) % (60 * 60) % 60;  // 时间戳中不足一分钟的秒数
+        // 从1970年开始，判断改时间戳可以组成多少个整年，同时除去整年占的天数
+        while (days > 364) {
+            if ((year % 100) != 0) {
+                if ((year % 4) == 0) {
+                    days -= 366;
+                    year++;
+                } else {
+                    days -= 365;
+                    year++;
+                }
+            } else {
+                if ((year % 400) == 0) {
+                    days -= 366;
+                    year++;
+                } else {
+                    days -= 365;
+                    year++;
+                }
+            }
+        }
+        // 判断剩下的天数可以组成多少个整月，并除去组成整月的天数
+        while ((days - dayToMonth[month]) > 0) {
+            month++;
+        }
+        days -= dayToMonth[month - 1];
+        // 得到换算为标准格式时间的字符串，并换算成期望的地区时间
+        this.time = year + "-" + month + "-" + (days + 1) + " " + leftHour + ":" + leftMinute + ":" + second;
+        toWhatType("U.S", currentType);
+        toWhatType(currentType, aimType);
+        days = Integer.parseInt(this.time.split("-")[2].split(" ")[0]);
+        leftHour = Integer.parseInt(this.time.split(":")[0].split(" ")[1]);
+        // 格式化输出时间，补充上前导零
+        System.out.printf("%s %s <--> %s-%02d %02d:%02d:%02d %s\n",
+                this.timestamp, currentType,
+                this.time.split("-")[0] + "-" + this.time.split("-")[1],
+                days, leftHour, leftMinute, second, aimType);
     }
 
-    public ExchangeOfTime() {
+
+
+    // 把标准格式时间转换为时间戳
+    private void timeToTimestamp(String time, String currentType, String aimType) {
+        this.time = time;
+        toWhatType(currentType, "U.S");
+        toWhatType(currentType, aimType);
+        // 将标准格式时间的字符串中的日期相关信息截取出来
+        int[] dates = new int[6];
+        for (int i = 0; i < dates.length; i++) {
+            if (i < 2) {
+                dates[i] = Integer.parseInt(this.time.split("-")[i]);
+            } else if (i == 2) {
+                dates[i] = Integer.parseInt(this.time.split("-")[2].split(" ")[0]);
+            } else {
+                dates[i] = Integer.parseInt(this.time.split(" ")[1].split(":")[i - 3]);
+            }
+        }
+        long timestamp = 0;
+        // 计算出从1970年到现在日期的整年份所占的秒数
+        for (int i = 1970; i < dates[0]; i++) {
+            if ((i % 100) != 0) {
+                if ((i % 4) == 0) {
+                    timestamp += (366 * 24 * 60 * 60);
+                } else {
+                    timestamp += (365 * 24 * 60 * 60);
+                }
+            } else {
+                if ((i % 400) == 0) {
+                    timestamp += (366 * 24 * 60 * 60);
+                } else {
+                    timestamp += (365 * 24 * 60 * 60);
+                }
+            }
+        }
+        // 加上不足一年的整月份所占的秒数，同时加上剩余不足一个月以及一天和一分钟的秒数
+        if ((dates[0] % 100) != 0) {
+            if (dates[0] % 4 == 0) {
+                if (dates[1] > 2) {
+                    timestamp = timestamp + (getDays(dates[1]) + 1 + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+                } else {
+                    timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+                }
+            } else {
+                timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+            }
+        } else {
+            if (dates[0] % 400 == 0) {
+                if (dates[1] > 2) {
+                    timestamp = timestamp + (getDays(dates[1]) + 1 + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+                } else {
+                    timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+                }
+            } else {
+                timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
+            }
+        }
+        // 得到最终的时间戳
+        System.out.println(timestamp + " " + aimType + " <--> " + time + " " + currentType);
     }
+
+
+    // 时差的转换
 
     // 北京时间和其他时间的互换
     private void toWhatType(String currentType, String aimType) {
@@ -86,7 +204,7 @@ public class ExchangeOfTime {
         }
     }
 
-    // 将北京时间转换为其他地区时间
+    // 将字符串中的北京时间转换为其他地区时间
     private void toLow(int lag) {
         if (Integer.parseInt(this.time.split(":")[0].split(" ")[1]) >= 8) {
             this.time = this.time.split(" ")[0] + " "
@@ -103,7 +221,7 @@ public class ExchangeOfTime {
         }
     }
 
-    // 将其他地区时间转换为北京时间
+    // 将字符串中的其他地区时间转换为北京时间
     private void toHigh(int lag) {
         if ((Integer.parseInt(this.time.split(":")[0].split(" ")[1]) + lag) < 24) {
             this.time = this.time.split(" ")[0] + " "
@@ -119,103 +237,7 @@ public class ExchangeOfTime {
                     + this.time.split(":")[2];
         }
     }
-    // 把时间戳转化为标准时间格式
-    private void timestampToTime(long timestamp, String currentType, String aimType) {
-        this.timestamp = timestamp;
-        long days = this.timestamp / (24 * 60 * 60);  // 天
-        int leftHour = (int) this.timestamp % (24 * 60 * 60) / (60 * 60);  // 时
-        int leftMinute = (int) this.timestamp % (24 * 60 * 60) % (60 * 60) / 60;  // 分
-        int year = 1970;
-        int month = 0;
-        int second = (int) this.timestamp % (24 * 60 * 60) % (60 * 60) % 60;
-        while (days > 364) {
-            if ((year % 100) != 0) {
-                if ((year % 4) == 0) {
-                    days -= 366;
-                    year++;
-                } else {
-                    days -= 365;
-                    year++;
-                }
-            } else {
-                if ((year % 400) == 0) {
-                    days -= 366;
-                    year++;
-                } else {
-                    days -= 365;
-                    year++;
-                }
-            }
-        }
-        while ((days - dayToMonth[month]) > 0) {
-            month++;
-        }
-        days -= dayToMonth[month - 1];
-        this.time = year + "-" + month + "-" + (days + 1) + " " + leftHour + ":" + leftMinute + ":" + second;
-        toWhatType("U.S", currentType);
-        toWhatType(currentType, aimType);
-        days = Integer.parseInt(this.time.split("-")[2].split(" ")[0]);
-        leftHour = Integer.parseInt(this.time.split(":")[0].split(" ")[1]);
-        // 格式化输出时间
-        System.out.printf("%s %s <--> %s-%02d %02d:%02d:%02d %s\n",
-                this.timestamp, currentType,
-                this.time.split("-")[0] + "-" + this.time.split("-")[1],
-                days, leftHour, leftMinute, second, aimType);
-    }
-    // 把标准格式时间转换为时间戳
-    private void timeToTimestamp(String time, String currentType, String aimType) {
-        this.time = time;
-        toWhatType(currentType, "U.S");
-        toWhatType(currentType, aimType);
-        int[] dates = new int[6];
-        for (int i = 0; i < dates.length; i++) {
-            if (i < 2) {
-                dates[i] = Integer.parseInt(this.time.split("-")[i]);
-            } else if (i == 2) {
-                dates[i] = Integer.parseInt(this.time.split("-")[2].split(" ")[0]);
-            } else {
-                dates[i] = Integer.parseInt(this.time.split(" ")[1].split(":")[i - 3]);
-            }
-        }
-        long timestamp = 0;
-        for (int i = 1970; i < dates[0]; i++) {
-            if ((i % 100) != 0) {
-                if ((i % 4) == 0) {
-                    timestamp += (366 * 24 * 60 * 60);
-                } else {
-                    timestamp += (365 * 24 * 60 * 60);
-                }
-            } else {
-                if ((i % 400) == 0) {
-                    timestamp += (366 * 24 * 60 * 60);
-                } else {
-                    timestamp += (365 * 24 * 60 * 60);
-                }
-            }
-        }
-        if ((dates[0] % 100) != 0) {
-            if (dates[0] % 4 == 0) {
-                if (dates[3] > 2) {
-                    timestamp = timestamp + (getDays(dates[1]) + 1 + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-                } else {
-                    timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-                }
-            } else {
-                timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-            }
-        } else {
-            if (dates[0] % 400 == 0) {
-                if (dates[3] > 2) {
-                    timestamp = timestamp + (getDays(dates[1]) + 1 + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-                } else {
-                    timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-                }
-            } else {
-                timestamp = timestamp + (getDays(dates[1]) + dates[2] - 1) * 24 * 60 * 60 + (dates[3] * 60 + dates[4]) * 60 + dates[5];
-            }
-        }
-        System.out.println(timestamp + " " + aimType + " <--> " + time + " " + currentType);
-    }
+    // 在换算为时间戳是将整月份的天数选择出来
     private int getDays(int month) {
         int days = 0;
         switch (month) {
