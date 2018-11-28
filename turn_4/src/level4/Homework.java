e51c0c152de4c4c0b40f5dfed82d0433164436e1
2564800726 <2564800726@qq.com>
2018-11-28 20:38:50 +0800

    红岩作业

--- turn_4/src/level4/Homework.java
+++ turn_4/src/level4/Homework.java
@@ -3,7 +3,7 @@ package level4;
 public class Homework {
     public static void main(String[] args) {
         MyJson json = new MyJson();
-        if (json.load("C:\\Users\\yb\\Desktop\\HomeworkOfAndroid\\turn_4\\src\\level4\\json1.txt")) {
+        if (json.load("turn_4\\src\\level4\\json1.txt")) {
             json.showInfo();
         }
     }
